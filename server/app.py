eccc0a8e1ae1b5b9a4de5bd4eed3e9848a4f49bc
Isaac Song <isaacsongs10@gmail.com>
2023-11-19 23:59:30 -0800

    password protection lab complete

--- server/app.py
+++ server/app.py
@@ -19,22 +19,38 @@ class Signup(Resource):
     
     def post(self):
         json = request.get_json()
-        user = User(
-            username=json['username'],
-            password_hash=json['password']
-        )
-        db.session.add(user)
-        db.session.commit()
-        return user.to_dict(), 201
+        if json['username'] and json['password']:
+            user = User(
+                username=json['username'],
+                password_hash=json['password']
+            )
+            db.session.add(user)
+            db.session.commit()
+
+            session['user_id'] = user.id
+            return user.to_dict(), 201
+        return {'error': 'Unprocessable entity'}, 422
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        if session.get('user_id'):
+            user = User.query.filter(User.id == session['user_id']).first()
+            return user.to_dict(), 200
+        return {}, 204
 
 class Login(Resource):
-    pass
+    def post(self):
+        json = request.get_json()
+        user = User.query.filter(User.username == json['username']).first()
+        if user.authenticate(json['password']):
+            session['user_id'] = user.id
+            return user.to_dict(), 201
+        return {'error': 'Unauthorized'}, 401
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
